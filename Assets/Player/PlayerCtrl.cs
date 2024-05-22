7320799e9ec2b2b101cc7d01d81f0a233bc7d438
rosarz <kanalcaspy@gmail.com>
2024-05-22 18:24:43 +0200

    animations set

--- Assets/Player/PlayerCtrl.cs
+++ Assets/Player/PlayerCtrl.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    public float movSpeed;
+    public Rigidbody2D rb;
 
-    public float movSpeed; 
-    public Rigidbody2D rb; 
+    private Animator anim;
+
+    //Jumping variables
+    public float jumpDuration;
+    public float jumpSpeedMultiplier;
+    private bool isJumping;
+    private float jumpEndTime;
+
+    private bool faceLeft = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -19,13 +30,68 @@ public class PlayerCtrl : MonoBehaviour
         float movX = Input.GetAxis("Horizontal");
         float movY = Input.GetAxis("Vertical");
 
-        if(Mathf.Abs(movX) > 0 )
+        Vector2 movement = new Vector2(movX, movY).normalized;
+
+        //Walking + animation 
+        if (movX > 0 && faceLeft)
         {
             rb.velocity = new Vector2(movX * movSpeed, rb.velocity.y);
+            anim.SetBool("isWalking", true);
+            Flip();
+            faceLeft = false;
         }
-        if (Mathf.Abs(movY) > 0 )
+        if (movY > 0 && !faceLeft)
         {
             rb.velocity = new Vector2(rb.velocity.x, movY * movSpeed);
+            anim.SetBool("isWalking", true);
+            Flip();
+            faceLeft = true;
         }
+
+        //Idle animation
+        if (movX == 0 && movY == 0)
+        {
+            anim.SetBool("isWalking", false);
+        }
+
+
+        //Attacking animation
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetBool("isAttacking", true);
+        }
+
+        //Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && !isJumping)
+        {
+            isJumping = true;
+            jumpEndTime = Time.time + jumpDuration;
+            anim.SetBool("isDashing", true);
+        }
+        if (isJumping)
+        {
+            rb.velocity = movement * movSpeed * jumpSpeedMultiplier;
+            if (Time.time > jumpEndTime)
+            {
+                isJumping = false;
+                anim.SetBool("isDashing", false);
+            }
+        }
+        else
+        {
+            rb.velocity = movement * movSpeed;
+        }
+    }
+
+    public void endAttack()
+    { 
+        anim.SetBool("isAttacking", false);
+    }
+
+    private void Flip()
+    {
+        var theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
