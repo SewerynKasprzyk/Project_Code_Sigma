850825b4dbef311008a991c350d3dc2fdddc5409
Koscystybaz <j.dudek294@gmail.com>
2024-05-23 21:53:18 +0200

    Inventory
    Working items adding to inv slots

--- Assets/Items/InventoryManager.cs
+++ Assets/Items/InventoryManager.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class InventoryManager : MonoBehaviour
 {
     public GameObject inventoryPanel;
+    public ItemSlot[] itemSlot;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +23,16 @@ public class InventoryManager : MonoBehaviour
     public void AddItem(string itemName, Sprite itemSprite)
     {
         Debug.Log("Added " + itemName + " to inventory");
+
+        for (int i = 0; i < itemSlot.Length; i++)
+        {
+            if (itemSlot[i].isFull == false)
+            {
+                itemSlot[i].AddItem(itemName, itemSprite);
+                itemSlot[i].isFull = true;
+                return;
+            }
+        }
+        
     }
 }
--- Assets/Items/ItemSlot.cs
+++ Assets/Items/ItemSlot.cs
@@ -1,9 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemSlot : MonoBehaviour
 {
+    //Data
+    public string itemName;
+    public Sprite itemSprite;
+    public bool isFull;
+
+    //Slot
+    [SerializeField] private Image itemImage;
+
+
+    public void AddItem(string itemName, Sprite itemSprite)
+    {
+        this.itemName = itemName;
+        Debug.Log("acc1");
+        this.itemSprite = itemSprite;
+        Debug.Log("acc2");
+        itemImage.sprite = itemSprite;
+        Debug.Log("acc3");
+    }
+ 
+
+
     // Start is called before the first frame update
     void Start()
     {
