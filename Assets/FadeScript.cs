f6960f89514311014c2c8b0a834fe3f9b61fe52c
SewerynKasprzyk <sewkas.studies@gmail.com>
2024-05-23 04:54:34 +0200

    Intro Animations added, animated slightly menu and added play and quit functionalities.

---
+++ Assets/FadeScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeScript : MonoBehaviour
+{
+
+    [SerializeField] private CanvasGroup myUIGroup;
+
+    [SerializeField] private bool fadeIn = false;
+    [SerializeField] private bool fadeOut = false;
+
+    public void ShowUI()
+    {
+        fadeIn = true;
+    }
+
+    public void HideUI()
+    {
+        fadeOut = true;
+    }
+
+    private void Update()
+    {
+        if (fadeIn)
+        {
+            if (myUIGroup.alpha < 1)
+            {
+                myUIGroup.alpha += Time.deltaTime;
+                if (myUIGroup.alpha >= 1)
+                {
+                    fadeIn = false;
+                }
+            }
+        }
+        if (fadeOut)
+        {
+            if (myUIGroup.alpha >= 0)
+            {
+                myUIGroup.alpha -= Time.deltaTime;
+                if (myUIGroup.alpha == 0)
+                {
+                    fadeOut = false;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Menu/MenuTimeEventHandler.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuTimeEventHandler : MonoBehaviour
+{
+    [SerializeField] private FadeScript fadeScriptMenuVideoCanvas;
+    [SerializeField] private FadeScript fadeScriptMenuCanvas;
+
+    private void Start()
+    {
+        FunctionTimer.CreateTimer(() => fadeScriptMenuVideoCanvas.ShowUI(), 0.5f, "ShowMenu");
+        FunctionTimer.CreateTimer(() => fadeScriptMenuCanvas.ShowUI(), 2f, "ShowMenu");
+
+        //FunctionTimer.StopTimer("Timer");
+    }
+
+    private void TestingAction()
+    {
+        //Debug.Log("Timer");
+    }
+
+    private void TestingAction_2()
+    {
+        //Debug.Log("Timer_2");
+    }
+
+}
